1b31f1af4d5489df9fb7d87e84a6ab1316bd7abc
Jesus Alberto Ortega Hernández <jesus.ortega103@alu.ulpgc.es>
2023-12-30 20:55:15 +0000

    Creation of class SQLiteTrackLoader.

---
+++ src/main/java/software/ulpgc/kata4/SQLiteTrackLoader.java
@@ -0,0 +1,43 @@
+package software.ulpgc.kata4;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class SQLiteTrackLoader implements TrackLoader{
+
+    private final Connection connection;
+    private final static String SQL = "select tracks.name as track, composer, Milliseconds, title, artists.Name as artist from tracks, albums, artists, genres where tracks.AlbumId = albums.AlbumId and albums.ArtistId = artists.ArtistId and tracks.GenreId = genres.GenreId";
+
+    public SQLiteTrackLoader(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public List<Track> loadAll() {
+        try {
+            return load(queryAll());
+        }
+        catch (SQLException e);
+        return Collections.emptyList();
+    }
+
+    private ResultSet queryAll() throws SQLException {
+        return connection.createStatement().executeQuery(SQL);
+    }
+
+    private static Track trackFrom(ResultSet resultSet) throws SQLException{
+        return new Track(
+                resultSet.getString("Track"),
+                resultSet.getString("composer"),
+                resultSet.getString("Title"),
+                resultSet.getString("artist"),
+                resultSet.getInt("milliseconds") / 100
+        );
+    }
+
+
+}
