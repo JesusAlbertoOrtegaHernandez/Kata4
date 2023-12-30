53772ebca8353a1f08881f7a665a29b6134ea5d0
Jesus Alberto Ortega Hernández <jesus.ortega103@alu.ulpgc.es>
2023-12-30 21:18:37 +0000

    Creation of class Main.

--- src/main/java/software/ulpgc/kata4/SQLiteTrackLoader.java
+++ src/main/java/software/ulpgc/kata4/SQLiteTrackLoader.java
@@ -4,12 +4,10 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class SQLiteTrackLoader implements TrackLoader{
-
+public class SQLiteTrackLoader implements TrackLoader {
     private final Connection connection;
     private final static String SQL = "select tracks.name as track, composer, Milliseconds, title, artists.Name as artist from tracks, albums, artists, genres where tracks.AlbumId = albums.AlbumId and albums.ArtistId = artists.ArtistId and tracks.GenreId = genres.GenreId";
 
@@ -21,32 +19,32 @@ public class SQLiteTrackLoader implements TrackLoader{
     public List<Track> loadAll() {
         try {
             return load(queryAll());
+        } catch (SQLException e) {
+            return Collections.emptyList();
         }
-        catch (SQLException e);
-        return Collections.emptyList();
     }
 
-    private List<Track> load(ResultSet resultSet)throws SQLException {
+    private List<Track> load(ResultSet resultSet) throws SQLException {
         List<Track> result = new ArrayList<>();
         while (resultSet.next())
             result.add(trackFrom(resultSet));
         return result;
-
     }
 
-    private ResultSet queryAll() throws SQLException {
-        return connection.createStatement().executeQuery(SQL);
-    }
-
-    private static Track trackFrom(ResultSet resultSet) throws SQLException{
+    private static Track trackFrom(ResultSet resultSet) throws SQLException {
         return new Track(
-                resultSet.getString("Track"),
+                resultSet.getString("track"),
                 resultSet.getString("composer"),
-                resultSet.getString("Title"),
+                resultSet.getString("title"),
                 resultSet.getString("artist"),
-                resultSet.getInt("milliseconds") / 100
+                resultSet.getInt("milliseconds") / 1000
         );
     }
 
+    private ResultSet queryAll() throws SQLException {
+        return connection
+                .createStatement()
+                .executeQuery(SQL);
+    }
 
 }
